382c8d7d9c81b32b9071eb2791a5ba2b43954297
Idealisten <prometheus.mr.cy@gmail.com>
2020-03-22 19:43:35 +0800

    一次学完指定学分所有课程，0.1/0.1/1.2学分测试通过

--- study_credit_course.py
+++ study_credit_course.py
@@ -6,7 +6,6 @@ from json import loads
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import selenium
 
 credit_list = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7,
                1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.9,
@@ -16,11 +15,10 @@ credit_list = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7,
 
 fail_num = 0
 success_num = 0
-select_credit = 0
-success_study = 0
-fail_study = 0
+fail_list = []
 course_info_list = []
-i = 1
+course_id = ''
+course_name = ''
 
 header = {
         'Accept': 'application/json, text/javascript, */*; q=0.01',
@@ -46,6 +44,8 @@ data_double = {
     'progress_measure': '100'
 }
 
+cookie = {}
+
 template_url = "http://study.foton.com.cn/els/html/coursestudyrecord/coursestudyrecord.studyCheck.do?courseId={}&scoId={}"
 progress_url = "http://study.foton.com.cn/els/html/courseStudyItem/courseStudyItem.saveProgress.do"
 precent_url = "http://study.foton.com.cn/els/html/courseStudyItem/courseStudyItem.saveCoursePrecent.do"
@@ -53,22 +53,27 @@ template_watch_url = "http://study.foton.com.cn/els/html/courseStudyItem/courseS
 template_play_url = "http://study.foton.com.cn/els/html/studyCourse/studyCourse.enterCourse.do?courseId={}&studyType=STUDY"
 
 def open_broswer():
-    global select_credit
-
-    print("课程学分：" + str(credit_list))
-    select_credit = input("请输入要选课的学分：")
-
-    print("正在打开登录页面，请登录后进入课程视频播放页面，然后回到程序输入ok继续执行")
+    print("正在打开登录页面，请登录后回到程序输入ok继续执行")
 
     driver.implicitly_wait(10)
     driver.get("http://study.foton.com.cn")
     driver.maximize_window()
 
 
-def study():
+def load_course():
     global select_credit
-    global success_num
-    global fail_num
+    with open('./course_data.txt', 'r', encoding='utf-8') as f:
+        line = f.readline()
+        line = f.readline()
+        while line:
+            line_list = line.strip().split(',')
+            credit = line_list[-2]
+            if credit == select_credit:
+                course_info_list.append(line)
+            line = f.readline()
+
+
+def judge():
     while True:
         ok = input("输入ok开始学习，输入no退出学习：")
         if ok == 'ok':
@@ -76,123 +81,164 @@ def study():
         else:
             driver.quit()
             exit(0)
-    driver.switch_to.window(driver.window_handles[1])
 
-    global i
-    with open('./course_data.txt', 'r', encoding='utf-8') as f:
-        line = f.readline()
-        line = f.readline()
-        while line:
-            course_info_list.append(line)
-            line = f.readline()
 
-    for course_info in course_info_list:
+def is_finished():
+    global success_num
+    try:
+        # 通过课程进度判断课程是否学习完成，span.text == '100'说明已经学完了
+        # span = driver.find_element_by_id("studyProgress")
+        span = WebDriverWait(driver, 3, 0.5).until(
+            EC.presence_of_element_located((By.ID, 'studyProgress')))
+    except:
+        try:
+            # 通过是否显示课程评估页面判断课程是否学习完成
+            # h1 = driver.find_element_by_class_name("main_title")
+            h1 = WebDriverWait(driver, 3, 0.5).until(
+                EC.presence_of_element_located((By.CLASS_NAME, 'main_title')))
+        except:
+            # 不显示课程评估，没学完，开始学习
+            learn()
+        else:
+            span = h1.find_element_by_tag_name('span')
+            if span.text == "课程评估":
+                success_num += 1
+                print("恭喜你！课程《{}》 已经完成学习，已成功学习 {} 门".format(course_name, success_num))
+
+    else:
+        if span.text == '100':
+            success_num += 1
+            print("恭喜你！课程《{}》 已经完成学习，已成功学习 {} 门".format(course_name, success_num))
+        else:
+            # 开始学习
+            learn()
+
+
+def get_cookie():
+    cookie_list = driver.get_cookies()
+    for single_cookie in cookie_list:
+        cookie[single_cookie['name']] = single_cookie['value']
+
+
+def learn():
+    global success_num
+    global fail_num
+    play_button = WebDriverWait(driver, 3, 0.5).until(
+        EC.presence_of_element_located((By.ID, 'courseRp_sel')))
+    play_button.click()
+    sleep(1)
+    get_cookie()
+    data_single['courseId'] = course_id
+    data_double['courseId'] = course_id
+
+    try:
+        # ele存在说明是双分屏或者三分屏
+        # ele = driver.find_element_by_id('vodtree')
+        ele = WebDriverWait(driver, 3, 0.5).until(
+            EC.presence_of_element_located((By.ID, 'vodtree')))
+    except:
+        # ele不存在说明是单分屏
         sleep(0.1)
-        line_list = course_info.strip().split(',')
-        credit = line_list[-2]
-        if credit == select_credit:
-            course_id = line_list[-3]
-            course_name = line_list[-4]
-            play_url = template_play_url.format(course_id)
-            driver.switch_to.window(driver.window_handles[1])
-            driver.get(play_url)
-            sleep(1)
-            try:
-                # try成功说明此门课已选课还未学习
-                play_button = WebDriverWait(driver, 3, 0.5).until(EC.presence_of_element_located((By.ID, 'courseRp_sel')))
-            except:
-                # 否则说明已经学习完毕进入课程评估页面
-                h1 = driver.find_element_by_class_name("main_title")
-                span = h1.find_element_by_tag_name('span')
-                if span.text == "课程评估":
-                    print("恭喜你！单分屏课程《{}》 已经完成学习".format(course_name))
+        try:
+            r = post(precent_url, headers=header, cookies=cookie, data=data_single, timeout=(15, 15))
+        except:
+            fail_num += 1
+            print("单分屏课程《{}》学习失败，已学习失败{}门课程".format(course_name, fail_num))
+            fail_list.append(course_name)
+        else:
+            r_data = r.text
+            if len(r_data) != 0:
+                r_dict = loads(r_data)
+                if 'completed' in r_dict:
+                    if r_dict['completed'] == 'true':
+                        success_num += 1
+                        print("恭喜你！课程《{}》 已经完成学习，已成功学习 {} 门".format(course_name, success_num))
             else:
-                # 如果还未学习则进入视频播放页面开始学习
-                play_button.click()
-                sleep(1)
-                cookie_list = driver.get_cookies()
-                cookie = {}
-                for single_cookie in cookie_list:
-                    cookie[single_cookie['name']] = single_cookie['value']
+                fail_num += 1
+                print("单分屏课程《{}》学习失败，已学习失败{}门课程".format(course_name, fail_num))
+                fail_list.append(course_name)
+    else:
+        # 双/三分屏
+        vid_list = []
+        title_list = []
+        div_list = ele.find_elements_by_tag_name('div')
+        for div in div_list[1:]:
+            try:
+                a = div.find_element_by_tag_name('a')
+            except BaseException:
+                pass
+            else:
+                video_id = a.get_attribute('data-id')
+                vid_list.append(video_id)
+                video_title = a.get_attribute('title')
+                title_list.append(video_title)
+                print("正在爬取 {} 视频数据...".format(video_title))
+                stdout.flush()
+        print("课程《{}》所有视频数据爬取完成！开始学习".format(course_name))
 
-                data_single['courseId'] = course_id
-                data_double['courseId'] = course_id
-                vid_list = []
-                title_list = []
-                try:
-                    # ele存在说明是双分屏或者三分屏
-                    ele = driver.find_element_by_id('vodtree')
-                except:
-                    # ele不存在说明是单分屏
-                    r = post(precent_url, headers=header, cookies=cookie, data=data_single, timeout=(15, 15))
-                    r_data = r.text
+        for index, vid in enumerate(vid_list):
+            title = title_list[index]
+            data_double['scoId'] = vid
+            video_url = template_url.format(course_id, vid)
+            try:
+                post(video_url, headers=header, cookies=cookie,
+                     data={'elsSign': cookie['eln_session_id']}, timeout=(15, 15))
+                sleep(0.2)
+                r = post(progress_url, headers=header, cookies=cookie, data=data_double, timeout=(15, 15))
+            except:
+                fail_num += 1
+                print("单分屏课程《{}》学习失败，已学习失败{}门课程".format(course_name, fail_num))
+                fail_list.append(course_name)
+            else:
+                r_data = r.text
+                if len(r_data) != 0:
                     r_dict = loads(r_data)
-                    if r_dict is not None:
-                        if 'completed' in r_dict:
-                            if r_dict['completed'] == 'true':
-                                print("恭喜你！单分屏课程《{}》 已经完成学习".format(course_name))
-                                stdout.flush()
-                                success_num += 1
-                            else:
-                                print("单分屏课程《{}》 学习失败".format(course_name))
-                                fail_num += 1
-                        else:
-                            print("单分屏课程《{}》 学习失败".format(course_name))
-                            fail_num += 1
-                    else:
-                        print("单分屏课程《{}》 学习失败".format(course_name))
-                        fail_num += 1
-                else:
-                    div_list = ele.find_elements_by_tag_name('div')
-                    for div in div_list[1:]:
-                        try:
-                            a = div.find_element_by_tag_name('a')
-
-                        except BaseException:
-                            pass
-                        else:
-                            video_id = a.get_attribute('data-id')
-                            vid_list.append(video_id)
-                            video_title = a.get_attribute('title')
-                            title_list.append(video_title)
-                            print("正在爬取 {} 视频数据...".format(video_title))
+                    if 'completed' in r_dict:
+                        if r_dict['completed'] == 'true':
+                            print("{} 已经完成学习".format(title))
                             stdout.flush()
-                    print("所有视频数据爬取完成！开始学习")
+                    if 'courseProgress' in r_dict:
+                        if r_dict['courseProgress'] == "100":
+                            success_num += 1
+                            print("恭喜你！课程《{}》 已经完成学习，已成功学习 {} 门".format(course_name, success_num))
+                else:
+                    fail_num += 1
+                    print("单分屏课程《{}》学习失败，已学习失败{}门课程".format(course_name, fail_num))
+                    fail_list.append(course_name)
+                sleep(0.2)
 
-                    for index, vid in enumerate(vid_list):
-                        title = title_list[index]
-                        data_double['scoId'] = vid
-                        video_url = template_url.format(course_id, vid)
-                        post(video_url, headers=header, cookies=cookie,
-                             data={'elsSign': cookie['eln_session_id']}, timeout=(15, 15))
-                        sleep(0.2)
-                        r = post(progress_url, headers=header, cookies=cookie, data=data_double, timeout=(15, 15))
-                        r_data = r.text
-                        r_dict = loads(r_data)
-                        if r_dict is not None:
-                            if 'completed' in r_dict:
-                                if r_dict['completed'] == 'true':
-                                    print("{} 已经完成学习".format(title))
-                                    stdout.flush()
-                            if 'courseProgress' in r_dict:
-                                if r_dict['courseProgress'] == "100":
-                                    print("恭喜你！双分屏课程《{}》所有视频已学习完毕".format(course_name))
-                                    success_num += 1
-                                else:
-                                    print("双分屏课程《{}》 学习失败".format(course_name))
-                                    fail_num += 1
-                            else:
-                                print("双分屏课程《{}》 学习失败".format(course_name))
-                                fail_num += 1
-                        sleep(0.2)
-    print("学习结束。学习成功{}门，学习失败{}门".format(success_num, fail_num))
 
-    # c = post(video_url, headers=header, cookies=cookie)
-    # print(c.status_code)
+def end_study():
+    print("本次学习结束，共{}门课程".format(len(course_info_list)))
+    print("学习成功{}门，学习失败{}门".format(success_num, fail_num))
+    if fail_num > 0:
+        print("学习失败的课程有{}".format(fail_list))
+        print("请检查学习失败的课程是否已经完成选课，未进行选课的课程无法学习。")
 
 
 if __name__ == "__main__":
-    driver = webdriver.Firefox()
-    open_broswer()
-    study()
-    driver.quit()
+    print("课程学分：" + str(credit_list))
+    select_credit = input("请输入要学习的课程的学分：")
+    if str(select_credit) not in credit_list:
+        print("输入错误。告辞")
+        exit(0)
+    else:
+        load_course()
+        driver = webdriver.Firefox()
+        open_broswer()
+        judge()
+        for course_info in course_info_list:
+            course_line_list = course_info.strip().split(',')
+            course_id = course_line_list[-3]
+            course_name = course_line_list[-4]
+            play_url = template_play_url.format(course_id)
+            driver.switch_to.window(driver.window_handles[0])
+            driver.get(play_url)
+            sleep(1)
+            try:
+                is_finished()
+            except:
+                print("请检查《{}》是否已选课！".format(course_name))
+        driver.quit()
+        end_study()
+
